76cd10f206ded99c4db6d79ee699e807115d784e
Riley Smith <riley.smith317@yahoo.com>
2020-09-03 17:37:47 -0500

    Finished tests

--- DataTests/UnitTests/EntreeTests/SmokehouseSkeletonTests.cs
+++ DataTests/UnitTests/EntreeTests/SmokehouseSkeletonTests.cs
@@ -6,59 +6,92 @@
 using Xunit;
 
 using BleakwindBuffet.Data;
+using BleakwindBuffet.Data.Entrees;
 
 namespace BleakwindBuffet.DataTests.UnitTests.EntreeTests
 {
     public class SmokehouseSkeletonTests
     {        
         [Fact]
-        public void ShouldInlcudeSausageByDefault()
+        public void ShouldIncludeSausageByDefault()
         {
+            SmokehouseSkeleton ss = new SmokehouseSkeleton();
+            Assert.True(ss.SausageLink);
         }
 
         [Fact]
-        public void ShouldInlcudeEggByDefault()
+        public void ShouldIncludeEggByDefault()
         {
+            SmokehouseSkeleton ss = new SmokehouseSkeleton();
+            Assert.True(ss.Egg);
         }
 
         [Fact]
-        public void ShouldInlcudeHashbrownsByDefault()
+        public void ShouldIlncludeHashbrownsByDefault()
         {
+            SmokehouseSkeleton ss = new SmokehouseSkeleton();
+            Assert.True(ss.Hashbrown);
         }
 
         [Fact]
-        public void ShouldInlcudePancakeByDefault()
+        public void ShouldIncLudePancakeByDefault()
         {
+            SmokehouseSkeleton ss = new SmokehouseSkeleton();
+            Assert.True(ss.Pancake);
         }
 
         [Fact]
         public void ShouldBeAbleToSetSausage()
         {
+            SmokehouseSkeleton ss = new SmokehouseSkeleton();
+            ss.SausageLink = true;
+            Assert.True(ss.SausageLink);
+            ss.SausageLink = false;
+            Assert.False(ss.SausageLink);
         }
 
         [Fact]
         public void ShouldBeAbleToSetEgg()
         {
+            SmokehouseSkeleton ss = new SmokehouseSkeleton();
+            ss.Egg = true;
+            Assert.True(ss.Egg);
+            ss.Egg = false;
+            Assert.False(ss.Egg);
         }
 
         [Fact]
         public void ShouldBeAbleToSetHashbrowns()
         {
+            SmokehouseSkeleton ss = new SmokehouseSkeleton();
+            ss.Hashbrown = true;
+            Assert.True(ss.Hashbrown);
+            ss.Hashbrown = false;
+            Assert.False(ss.Hashbrown);
         }
 
         [Fact]
         public void ShouldBeAbleToSetPancake()
         {
+            SmokehouseSkeleton ss = new SmokehouseSkeleton();
+            ss.Pancake = true;
+            Assert.True(ss.Pancake);
+            ss.Pancake = false;
+            Assert.False(ss.Pancake);
         }
 
         [Fact]
         public void ShouldReturnCorrectPrice()
         {
+            SmokehouseSkeleton ss = new SmokehouseSkeleton();
+            Assert.Equal(5.62, ss.Price);
         }
 
         [Fact]
         public void ShouldReturnCorrectCalories()
         {
+            SmokehouseSkeleton ss = new SmokehouseSkeleton();
+            Assert.Equal((uint)602, ss.Calories);
         }
 
         [Theory]
@@ -67,11 +100,23 @@ namespace BleakwindBuffet.DataTests.UnitTests.EntreeTests
         public void ShouldReturnCorrectSpecialInstructions(bool includeSausage, bool includeEgg,
                                                             bool includeHashbrowns, bool includePancake)
         {
+            SmokehouseSkeleton ss = new SmokehouseSkeleton();
+            ss.SausageLink = includeSausage;
+            ss.Egg = includeEgg;
+            ss.Hashbrown = includeHashbrowns;
+            ss.Pancake = includePancake;
+            if (!includeSausage) Assert.Contains("Hold sausage", ss.SpecialInstructions);
+            if (!includeEgg) Assert.Contains("Hold eggs", ss.SpecialInstructions);
+            if (!includeHashbrowns) Assert.Contains("Hold hash browns", ss.SpecialInstructions);
+            if (!includePancake) Assert.Contains("Hold pancakes", ss.SpecialInstructions);
+            if (includeSausage && includeEgg && includeHashbrowns && includePancake) Assert.Empty(ss.SpecialInstructions);
         }
 
         [Fact]
         public void ShouldReturnCorrectToString()
         {
+            SmokehouseSkeleton ss = new SmokehouseSkeleton();
+            Assert.Equal("Smokehouse Skeleton", ss.ToString());
         }
     }
 }
--- DataTests/UnitTests/SideTests/DragonbornWaffleFriesTests.cs
+++ DataTests/UnitTests/SideTests/DragonbornWaffleFriesTests.cs
@@ -7,6 +7,7 @@ using Xunit;
 
 using BleakwindBuffet.Data;
 using BleakwindBuffet.Data.Enums;
+using BleakwindBuffet.Data.Sides;
 
 namespace BleakwindBuffet.DataTests.UnitTests.SideTests
 {
@@ -15,17 +16,27 @@ namespace BleakwindBuffet.DataTests.UnitTests.SideTests
         [Fact]
         public void ShouldBeSmallByDefault()
         {
+            DragonbornWaffleFries dbwf = new DragonbornWaffleFries();
+            Assert.Equal(Size.Small, dbwf.Size);
         }
 
         [Fact]
         public void ShouldBeAbleToSetSize()
         {
+            DragonbornWaffleFries dbwf = new DragonbornWaffleFries();
+            dbwf.Size = Size.Large;
+            Assert.Equal(Size.Large, dbwf.Size);
+            dbwf.Size = Size.Medium;
+            Assert.Equal(Size.Medium, dbwf.Size);
+            dbwf.Size = Size.Small;
+            Assert.Equal(Size.Small, dbwf.Size);
         }
 
-        [Fact]
+ /*       [Fact]
         public void ShouldReturnCorrectSpecialInstructions()
         {
-        }
+
+        } */
 
         [Theory]
         [InlineData(Size.Small, 0.42)]
@@ -33,6 +44,9 @@ namespace BleakwindBuffet.DataTests.UnitTests.SideTests
         [InlineData(Size.Large, 0.96)]
         public void ShouldReturnCorrectPriceBasedOnSize(Size size, double price)
         {
+            DragonbornWaffleFries dbwf = new DragonbornWaffleFries();
+            dbwf.Size = size;
+            Assert.Equal(price, dbwf.Price);
         }
 
         [Theory]
@@ -41,6 +55,9 @@ namespace BleakwindBuffet.DataTests.UnitTests.SideTests
         [InlineData(Size.Large, 100)]
         public void ShouldReturnCorrectCaloriesBasedOnSize(Size size, uint calories)
         {
+            DragonbornWaffleFries dbwf = new DragonbornWaffleFries();
+            dbwf.Size = size;
+            Assert.Equal(calories, dbwf.Calories);
         }
 
         [Theory]
@@ -49,6 +66,9 @@ namespace BleakwindBuffet.DataTests.UnitTests.SideTests
         [InlineData(Size.Large, "Large Dragonborn Waffle Fries")]
         public void ShouldReturnCorrectToStringBasedOnSize(Size size, string name)
         {
+            DragonbornWaffleFries dbwf = new DragonbornWaffleFries();
+            dbwf.Size = size;
+            Assert.Equal(name, dbwf.ToString());
         }
     }
 }
