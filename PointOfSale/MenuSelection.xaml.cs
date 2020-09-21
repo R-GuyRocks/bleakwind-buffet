994e59718f5b4303a62526651ee6ddf823922a6b
riley317 <riley317@F1093-16-2275>
2020-09-21 16:24:08 -0500

    Added new changes.

--- PointOfSale/MenuSelection.xaml.cs
+++ PointOfSale/MenuSelection.xaml.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -10,24 +8,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace PointOfSale
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for MenuSelection.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MenuSelection : Window
     {
-        public MainWindow()
+        public MenuSelection()
         {
             InitializeComponent();
-        }
-
-        private void BriarheartBurgerClick(object sender, RoutedEventArgs e)
-        {
-
         }
     }
 }
